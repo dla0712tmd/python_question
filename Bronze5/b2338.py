d6338c0572369e84cfef92328722cf23c7daa0f2
dlagus0712 <>
2021-07-25 06:53:15 +0000

    20210724

---
+++ Bronze5/b2338.py
@@ -0,0 +1,6 @@
+a = int(input("첫번 째 수를 입력해주세요"))
+b = int(input("두번 째 수를 입력해주세요"))
+
+print(a + b)
+print(a - b)
+print(a * b)
---
+++ Bronze5/b2558.py
@@ -0,0 +1,4 @@
+a = int(input("첫번 째 수를 입력해주세요"))
+b = int(input("두번 째 수를 입력해주세요"))
+
+print(a + b)
